f18647cef19ef561e5cb8b5378dd271797e4640a
Shashank Tripathi <shashanktrp556@gmail.com>
2022-02-14 22:18:25 +0530

    update user detail endpoint

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from datetime import datetime, timedelta
 import json
 from fastapi import Depends, FastAPI , HTTPException
 from random import randint
 from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from schemas import User_in,baseUserModel
+from passlib.context import CryptContext
+from jose import jwt,JWTError
+from schemas import User_in,baseUserModel,Token
 import pymongo
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_secret_key():
     with open('secrets.json','r') as fh :
         js = json.load(fh)
         return js['SECRET_KEY']
 SECRET_KEY = get_secret_key()
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_DAYS=30
 
 # --------------------------------------------------------------------------------------------------
 
@@ -30,15 +36,50 @@ async def shutdown_event():
     mcl.close()
 
 # -----------------------------------------------------------------------------------------------------------------------------
+async def verify_password(plain_pass,hash_pass):
+    return pwd_context.verify(plain_pass,hash_pass)
 
-async def fake_password_hash(password):
-    pass
+async def get_password_hash(password):
+    return pwd_context.hash(password)
 
-async def decode_token(token):
-    return {}
+
+async def create_access_token(data: dict,expire_delta: timedelta = None):
+    to_encode = data.copy()
+
+    if expire_delta:
+        expire = datetime.utcnow()+ expire_delta
+    else :
+        expire = datetime.utcnow()+ timedelta(minutes=59)
+
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def authenticate_user(username: str, password: str):
+    user = user_coll.find_one({'username':username})
+    if not user:
+        return False
+    if not await verify_password(password, user['password']):
+        return False
+    return user
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = await decode_token(token)
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = user_coll.find_one({'username':username})
+    if user is None:
+        raise credentials_exception
     return user
 
 async def generate_username(name:str):
@@ -51,23 +92,25 @@ async def generate_username(name:str):
 # ------------------------------------------------------------------------------
 
 @app.post('/user/register',response_model=baseUserModel,response_model_exclude=['password'],status_code=201)
-async def register_users(user: User_in):
-    user = baseUserModel(**user.dict())
+async def register_users(user_in: User_in):
+    user = baseUserModel(**user_in.dict())
     user.username = await generate_username(user.name)
+    user.password = await get_password_hash(user.password)
     user_coll.insert_one(user.dict())
     return user
 
-@app.post('/token')
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = user_coll.find_one({'username':form_data.username})
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=400,detail='Given username does not exist')
-    
-    hash_password = fake_password_hash(form_data.password)
-    if not hash_password == user.password :
-        raise HTTPException(status_code=400,detail='Given password is incorrect')
-    
-    return {'Access_token':user.username}
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    access_token = await create_access_token(data={"sub": user['username']}, expire_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get('/user',response_model=baseUserModel,response_model_exclude=['password'])
--- schemas.py
+++ schemas.py
@@ -15,10 +15,14 @@ class roomModel_in(BaseModel) :
     name: str = Field(title='Name of the chat room',max_length=30)
 
 class roomModel_out(roomModel_in) :
-    code:str = Field(title="Unique code of the chatroom",max_length=8)
+    code:str = Field(title="Chatroom Code",description="Unique code to join the chatroom",max_length=8)
     msgs: List[dict] = Field([],title="All the messages in the chat room")
 
 class messageModel(BaseModel) :
-    msg: str = Field(title='Message sent by a user',max_length=300)
-    writer: str
-    created_at: datetime
+    msg: str = Field(title='The message',max_length=300)
+    writer: str = Field(title = 'Username',description='Username of the user who wrote sent the message')
+    created_at: datetime = Field(None)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
