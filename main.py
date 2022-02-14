28ec1dd3eee2bc0ad077d92440d0e52a2b8b4a59
Shashank Tripathi <shashanktrp556@gmail.com>
2022-02-14 11:40:18 +0530

    update login and Oauth

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from string import ascii_letters
-from fastapi import FastAPI
+import json
+from fastapi import Depends, FastAPI , HTTPException
 from random import randint
-from fastapi.security import OAuth2PasswordBearer
-from schemas import baseUserModel
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from schemas import User_in,baseUserModel
 import pymongo
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+def get_secret_key():
+    with open('secrets.json','r') as fh :
+        js = json.load(fh)
+        return js['SECRET_KEY']
+SECRET_KEY = get_secret_key()
+
+# --------------------------------------------------------------------------------------------------
 
 @app.on_event('startup')
 async def startup_envents():
@@ -20,18 +29,47 @@ async def startup_envents():
 async def shutdown_event():
     mcl.close()
 
+# -----------------------------------------------------------------------------------------------------------------------------
+
+async def fake_password_hash(password):
+    pass
+
+async def decode_token(token):
+    return {}
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = await decode_token(token)
+    return user
+
 async def generate_username(name:str):
     while(True) :
         num = randint(1,4)
-        temp = f"{name}{randint(1,99999)}"
+        temp = f"{''.join(name.split())}{randint(1,99999)}"
         if user_coll.find_one({'username':temp}) is None :
             return temp
 
+# ------------------------------------------------------------------------------
 
-@app.post('/api/user/register')
-async def register_users(user: baseUserModel):
+@app.post('/user/register',response_model=baseUserModel,response_model_exclude=['password'],status_code=201)
+async def register_users(user: User_in):
+    user = baseUserModel(**user.dict())
     user.username = await generate_username(user.name)
-    user.created_room,user.joined_room = [],[]
     user_coll.insert_one(user.dict())
     return user
-            
+
+@app.post('/token')
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = user_coll.find_one({'username':form_data.username})
+    if not user:
+        raise HTTPException(status_code=400,detail='Given username does not exist')
+    
+    hash_password = fake_password_hash(form_data.password)
+    if not hash_password == user.password :
+        raise HTTPException(status_code=400,detail='Given password is incorrect')
+    
+    return {'Access_token':user.username}
+
+
+@app.get('/user',response_model=baseUserModel,response_model_exclude=['password'])
+async def user_profile(user: baseUserModel = Depends(get_current_user)) :
+    return user
--- schemas.py
+++ schemas.py
@@ -2,12 +2,21 @@ from datetime import datetime
 from pydantic import BaseModel,Field
 from typing import List
 
-class baseUserModel(BaseModel) :
+class User_in(BaseModel) :
     name: str = Field(title="Full name of the user",max_length=30)
-    username: str = Field(None,title='Unique user name for user',max_length=40)
     password: str
+
+class baseUserModel(User_in) :
+    username: str = Field(None,title='Unique user name for user',max_length=40)
     created_room: List[str] = Field([])
     joined_room: List[str] = Field([])
+
+class roomModel_in(BaseModel) :
+    name: str = Field(title='Name of the chat room',max_length=30)
+
+class roomModel_out(roomModel_in) :
+    code:str = Field(title="Unique code of the chatroom",max_length=8)
+    msgs: List[dict] = Field([],title="All the messages in the chat room")
 
 class messageModel(BaseModel) :
     msg: str = Field(title='Message sent by a user',max_length=300)
